a6e8c7f9f8aa70bb2898fc05ca5aac787bdc152c
jean <jeanc.aldea1@gmail.com>
2020-03-03 02:16:44 -0300

    First commit

---
+++ Variables.js
@@ -0,0 +1,21 @@
+var nombre = "Jean";
+
+//declarar una variable en js
+var edad;
+
+edad
+
+//Inicializar una variable
+
+edad = 30;
+
+edad;
+
+var elementos = ["computador", 30]
+
+elementos;
+
+var elementos2 = {
+    nombre: "jean",
+    edad: 30
+}
---
+++ valores.js
@@ -0,0 +1,18 @@
+// Valores numericos
+40 
+//Valores de tipo String
+"Valor de tipo string"
+//Valores de tipo booleanod
+true 
+false 
+//Valores de tipo vacio
+null
+undefined //tener cuidado  con estes
+//Valores de tipo objeto:
+[1,2,"hola"];
+{
+    nombre: "Jean";
+}
+//nota: en el navegador para ver que tipo de valor es colocas typeof ejemplo:
+//typeof true
+//y te va decir boleaan
